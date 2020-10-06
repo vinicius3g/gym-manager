41c81fbb6ee2aa093e1d7d5c649b3592d096a725
vinicius.paixao <vinicius.paixao@acct.global>
2020-10-06 16:17:15 -0300

    instructors functions

---
+++ routes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const routes = express.Router()
+
+
+
+routes.get('/', function(req,res) {
+    res.redirect('/instructors')
+})
+routes.get('/instructors', function(req,res) {
+    res.render('instructors/index')
+})
+routes.get('/members', function(req,res) {
+    res.render('members')
+})
+routes.get('/instructors/create', function(req,res) {
+    res.render('instructors/create')
+})
+routes.post('/instructors', function(req,res) {
+    res.send('go')
+})
+
+module.exports = routes
